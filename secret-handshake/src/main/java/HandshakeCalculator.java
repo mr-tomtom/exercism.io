091521bfbe5b32a8afd377044c94b8d0eccf115f
Thomas Cornwall <a8964f40@opayq.com>
2017-03-30 11:41:41 +0700

    Testing enum itteration and need to redo maven setting in intelij so commit to this test branch

--- secret-handshake/src/main/java/HandshakeCalculator.java
+++ secret-handshake/src/main/java/HandshakeCalculator.java
@@ -9,26 +9,14 @@ final class HandshakeCalculator {
     public List<Signal> calculateHandshake(int decNum) {
 
         String binary = Integer.toBinaryString(decNum);
-        int bLen = binary.length();
         handShake = new ArrayList<>();
+        int i = 0;
 
-        if (bLen < 1) {
-            return Collections.emptyList();
-        }
-        if (bLen > 0 && binary.charAt(bLen-1) == '1') {
-            handShake.add(Signal.WINK);
-        }
-        if (bLen > 1 && binary.charAt(bLen-2) == '1') {
-            handShake.add(Signal.DOUBLE_BLINK);
-        }
-        if (bLen > 2 && binary.charAt(bLen-3) == '1') {
-            handShake.add(Signal.CLOSE_YOUR_EYES);
-        }
-        if (bLen > 3 && binary.charAt(bLen-4) == '1') {
-            handShake.add(Signal.JUMP);
-        }
-        if (bLen > 4 && binary.charAt(bLen-5) == '1') {
-            Collections.reverse(handShake);
+        for (Signal s : Signal.values()) {
+            if (binary.length() > i && binary.charAt(i) == '1') {
+                handShake.add(s);
+            }
+            i++;
         }
         return handShake;
     }
