b241f6f2ab8ea5770ba26ed07361f6255344280d
Thomas Cornwall <a8964f40@opayq.com>
2017-04-05 16:47:22 +0700

    initial matrix commit not complete

---
+++ matrix/src/main/java/Matrix.java
@@ -0,0 +1,27 @@
+
+public class Matrix {
+
+    public Matrix (String str) {
+
+    }
+
+    public int getRowsCount() {
+
+        return 0;
+    }
+
+    public int getColumnsCount() {
+
+        return 0;
+    }
+
+    public int[] getRow(int i) {
+
+        return new int[0];
+    }
+
+    public int[] getColumn(int i) {
+
+        return new int[0];
+    }
+}
